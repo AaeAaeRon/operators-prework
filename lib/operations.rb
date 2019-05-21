d4bac4d635528bdc83fbb572e9022ab06e77f982
Aaron B <abriscoe2008@gmail.com>
2019-05-21 05:30:36 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,11 +2,7 @@ def unsafe?(speed)
   if speed < 40 || speed > 60
     true 
   else 
-    def unsafe?(speed)
-  if speed < 40 || speed = > 60
-    puts  true 
-  else 
-    puts false 
+    false 
   end 
 end
 
